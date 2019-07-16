cbb3dd35ca1cfec11bde6f5a5a952678a82426c7
hyejun8701 <hyejun8701@gmail.com>
2019-07-16 13:42:22 +0900

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!',
+        list: ['사과', '바나나', '딸기'],
+        count: 1,
+        show: true,
+    },
+    methods: {
+        handleClick:function(event) {
+            alert(event.target)
+        }
+    }
+})
